d858cfdc1dc6fb3f1d5f39c7d74dafc451b0164f
Brenno Grangeiro <bhgrangeiro@gmail.com>
2026-06-04 17:26:38 -0300

    fix: Corrigindo código

--- exercicio2/testeVelocidade/script.js
+++ exercicio2/testeVelocidade/script.js
@@ -1,12 +1,12 @@
 alert("Teste de Velocidade")
 
 const nomeVeiculo1 = prompt("Digite o nome do primeiro veículo: ")
-const velVeiculo1 = prompt("Digite a sua velocidade: ")
+const velVeiculo1 = parseFloat(prompt("Digite a sua velocidade: "))
 
 const nomeVeiculo2 = prompt("Digite o nome do outro veículo: ")
-const velVeiculo2 = prompt("Agora digite sua velocidade: ")
+const velVeiculo2 = parseFloat(prompt("Agora digite sua velocidade: "))
 
-if (parseFloat(velVeiculo1) > parseFloat(velVeiculo2)) {
+if (velVeiculo1 > velVeiculo2) {
     alert(
     "Velocidades: " +
     "\n" + nomeVeiculo1 + " -> " + velVeiculo1 + " km/h" +
@@ -14,7 +14,7 @@ if (parseFloat(velVeiculo1) > parseFloat(velVeiculo2)) {
     "\n" +
     "\nO " + nomeVeiculo1 + " foi mais rápido!"
 )
-} else if (parseFloat(velVeiculo2) > parseFloat(velVeiculo1)) {
+} else if (velVeiculo2 > velVeiculo1) {
     alert(
     "Velocidades: " +
     "\n" + nomeVeiculo1 + " -> " + velVeiculo1 + " km/h" +
