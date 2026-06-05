2858d803279da7258a44a9ff2584e376177c53fd
Brenno Grangeiro <bhgrangeiro@gmail.com>
2026-06-05 16:02:48 -0300

    fix: Correção e atualização do Código

--- exercicio2/calculoDano/script.js
+++ exercicio2/calculoDano/script.js
@@ -1,39 +1,13 @@
-alert("Sistema de Calculo de Dano")
+alert("Sistema de Cálculo de Dano")
 
 const nomeAtacante = prompt("Digite o nome do Atacante: ")
 const danoAtacante = parseFloat(prompt("Agora digite a quantidade de dano desse atacante: "))
-let danoCausado = null
-
-alert(
-    "ATACANTE" +
-    "\n" +
-    "\nNome: " + nomeAtacante +
-    "\nDano: " + danoAtacante
-)
+let danoCausado = 0
 
 const nomeDefensor = prompt("Digite o nome do Defensor: ")
-const hpDefensor = parseFloat(prompt("Digite a quantidade de vida do Defensor: "))
+let hpDefensor = parseFloat(prompt("Digite a quantidade de vida do Defensor: "))
 const poderDefesa = parseFloat(prompt("Qual o poder de defesa do defensor: "))
-const escudo = confirm("Ele tem um escudo?")
-
-alert(
-    "DEFENSOR" +
-    "\n" +
-    "\nNome: " + nomeDefensor +
-    "\nQuantidade de HP: " + hpDefensor +
-    "\nPoder de Defesa: " + poderDefesa +
-    "\nPossui escudo? " + escudo
-)
-
-if (danoAtacante > poderDefesa && escudo === false) {
-    danoCausado = danoAtacante - poderDefesa
-} else if (danoAtacante > poderDefesa && escudo === true) {
-    danoCausado = (danoAtacante - poderDefesa) / 2
-} else if (danoAtacante <= poderDefesa) {
-    danoCausado = 0
-}
-
-const hpAtual = hpDefensor - danoCausado
+const escudo = prompt("Ele tem um escudo? (SIM/NÃO)")
 
 alert(
     "ATACANTE" +
@@ -41,15 +15,38 @@ alert(
     "\nNome: " + nomeAtacante +
     "\nDano: " + danoAtacante +
     "\n" +
-    "\nQuantidade de HP inicial: " + hpDefensor +
-    "\nPoder de defesa: " + poderDefesa + " VS Dano do atacante: " + danoAtacante + 
+    "\nDEFENSOR" +
     "\n" +
-    "\nDano Causado: " + danoCausado +
+    "\nNome: " + nomeDefensor +
+    "\nQuantidade de HP: " + hpDefensor +
+    "\nPoder de Defesa: " + poderDefesa +
+    "\nPossui escudo? " + escudo
+)
+
+if (danoAtacante > poderDefesa && escudo == "NÃO") {
+    danoCausado = danoAtacante - poderDefesa
+} else if (danoAtacante > poderDefesa && escudo == "SIM") {
+    danoCausado = (danoAtacante - poderDefesa) / 2
+} else if (danoAtacante <= poderDefesa) {
+    danoCausado = 0
+}
+
+hpDefensor -= danoCausado
+
+alert(
+    nomeAtacante + " causou " + danoCausado + " pontos de dano em " + nomeDefensor
+)
+
+alert(
+    "ATACANTE" +
+    "\n" +
+    "\nNome: " + nomeAtacante +
+    "\nDano: " + danoAtacante +
     "\n" +
     "\nDEFENSOR" +
     "\n" +
     "\nNome: " + nomeDefensor +
-    "\nQuantidade de HP Atual: " + hpAtual +
+    "\nQuantidade de HP: " + hpDefensor +
     "\nPoder de Defesa: " + poderDefesa +
     "\nPossui escudo? " + escudo
 )
